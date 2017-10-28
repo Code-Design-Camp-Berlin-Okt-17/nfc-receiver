141b3660f645a66ce92f734739603cb9e9791574
jneidel <jneidel@outlook.com>
2017-10-28 10:26:59 +0200

    Add sending capabilities

--- nfcreceiver/app/src/main/java/elocktronics/nfc_receiver/MainActivity.java
+++ nfcreceiver/app/src/main/java/elocktronics/nfc_receiver/MainActivity.java
@@ -2,15 +2,22 @@ package elocktronics.nfc_receiver;
 
 import android.content.Intent;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback, NfcAdapter.OnNdefPushCompleteCallback {
 
     private TextView textInput;
+    private String confirmation;
+    private String keyWithoutHash = "1234";
+    private boolean isSending = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,10 +35,44 @@ public class MainActivity extends AppCompatActivity {
                     NfcAdapter.EXTRA_NDEF_MESSAGES);
 
             NdefMessage message = (NdefMessage) rawMessages[0]; // only one message transferred
-            textInput.setText(new String(message.getRecords()[0].getPayload()));
+            String stringMessage = new String(message.getRecords()[0].getPayload());
+            Log.i("Done", stringMessage);
+            if(stringMessage.equals(keyWithoutHash)){
+                confirmation = "accepted";
+                textInput.setText("Accepted");
+                isSending = true;
+                nfcSend();
+            }else{
+                confirmation = "denied";
+                textInput.setText("Denied");
+                isSending = true;
+                nfcSend();
+            }
 
         } else
-            textInput.setText("Waiting for Transmittion.");
+            textInput.setText("Waiting for Transmittion...");
+    }
 
+    public void nfcSend() {
+        NfcAdapter mAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (!mAdapter.isEnabled()) {
+            Toast.makeText(this, "Please enable NFC via Settings.", Toast.LENGTH_LONG).show();
+        }
+
+        mAdapter.setNdefPushMessageCallback(this, this);
+        mAdapter.setOnNdefPushCompleteCallback(this, this);
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent nfcEvent) {
+        NdefRecord ndefRecord = NdefRecord.createMime("text/plain", confirmation.getBytes());
+        NdefMessage ndefMessage = new NdefMessage(ndefRecord);
+        return ndefMessage;
+    }
+
+    @Override
+    public void onNdefPushComplete(NfcEvent event) {
+        isSending = false;
     }
 }
